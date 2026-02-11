a99d2013dd1a4e910426ed177082ce961c457294
Anastasia Zaparaniuk <zaparaniuknastia@gmail.com>
2026-02-11 23:16:49 +0200

    merge feauter/user-access in main

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 console.log('Hello World!');
 
-<<<<<<< HEAD
 const userAge = 35;
 if (userAge <= 18) {
   console.log('Ви повнолітній')
 } else {
   console.log('Ви НЕ повнолітній')
-=======
-const age = 20;
-if (age <= 18) {
-  console.log('Ви повнолітній');
-} else {
-  console.log('Ви НЕ повнолітній');
->>>>>>> feauter/user-access
 };
